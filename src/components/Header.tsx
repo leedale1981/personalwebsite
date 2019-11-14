6d0682a72c004968b15bac2c93fae2c340644017
Lee Dale <lee.dale@ofqual.gov.uk>
2019-11-14 15:59:48 +0000

    Adding footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { Grid, Typography, IconButton, Avatar } from '@material-ui/core';
 import MeLogo from '../images/me.jpg';
 import AviationIcon from '../images/propeller.png';
 import TechIcon from '../images/macbook.png';
+import ScienceIcon from '../images/science.png';
 import ScubaIcon from '../images/mask.png';
 import BoatIcon from '../images/boat.png';
 import '../App.css';
@@ -10,33 +11,39 @@ import '../App.css';
 const Header: React.FC = () => {
   return (
     <Grid container spacing={0} alignItems='center' justify='flex-start' direction='row'>
-      <Grid item md={2} xs={2}>
+      <Grid item md={2} xs={6}>
         <Avatar alt="Lee Dale" src={MeLogo} className='Me-Avatar' />
       </Grid>
-      <Grid item md={2} xs={2} alignContent='flex-start' alignItems='flex-start'>
+      <Grid item md={2} xs={6} alignContent='flex-start' alignItems='flex-start'>
         <Typography variant='h4' className='Courier-Typography'>Lee Dale</Typography>
       </Grid>
       <Grid item md={8} xs={12}>
         <Grid container spacing={0} alignItems='center' justify='flex-start' direction='row'>
-          <Grid item md={3} xs={12}>
+          <Grid item md={2} sm={4} xs={6}>
             <IconButton style={{ backgroundColor: 'transparent' }}>
               <img src={TechIcon} className='Nav-Button' />
             </IconButton>
             <Typography variant='h6' className='Courier-Typography'>Technology</Typography>
           </Grid>
-          <Grid item md={3} xs={12}>
+          <Grid item md={2} sm={4} xs={6}>
+            <IconButton style={{ backgroundColor: 'transparent' }}>
+              <img src={ScienceIcon} className='Nav-Button' />
+            </IconButton>
+            <Typography variant='h6' className='Courier-Typography'>Science</Typography>
+          </Grid>
+          <Grid item md={2} sm={4} xs={6}>
             <IconButton style={{ backgroundColor: 'transparent' }}>
               <img src={AviationIcon} className='Nav-Button' />
             </IconButton>
             <Typography variant='h6' className='Courier-Typography'>Flying</Typography>
           </Grid>
-          <Grid item md={3} xs={12}>
+          <Grid item md={2} sm={4} xs={6}>
             <IconButton style={{ backgroundColor: 'transparent' }}>
               <img src={BoatIcon} className='Nav-Button' />
             </IconButton>
             <Typography variant='h6' className='Courier-Typography'>Sailing</Typography>
           </Grid>
-          <Grid item md={3} xs={12}>
+          <Grid item md={2} sm={4} xs={6}>
             <IconButton style={{ backgroundColor: 'transparent' }}>
               <img src={ScubaIcon} className='Nav-Button' />
             </IconButton>
